fafd9ef7fd5778d34940e031e70ba1b5421b4f56
NeoTyger <neojl333@gmail.com>
2023-02-12 22:21:20 +0100

    Pasar de level hecho.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,9 +11,11 @@ public class GameManager : MonoBehaviour
     private GameManager instance;
     [SerializeField] private TextMeshProUGUI _txtPoints;
 
-    private int points = 0;
+    private int points = 5;
+    private int collectedPoints = 0;
     private bool winner = false;
     private int level = 0;
+    
 
     private void Awake()
     {
@@ -31,85 +33,25 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        StartLevel();
-    }
-
-    private void StartLevel()
-    {
-        winner = false;
-        NextLevel();
-
-        if (!PlayerPrefs.HasKey("level"))
-        {
-            level = 1;
-            PlayerPrefs.SetInt("level", level);
-            PlayerPrefs.Save();
-        }
-        else
-        {
-            level = PlayerPrefs.GetInt("level");
-        }
-    }
-
-    private void NextLevel()
-    {
-        if (level == 1 && winner == true)
-        {
-            winner = false;
-            PlayerPrefs.SetInt("level", 2);
-            PlayerPrefs.Save();
-            SceneManager.LoadScene("Level 2");
-        }
-        else
-        {
-            PlayerPrefs.SetInt("level", 1);
-            PlayerPrefs.Save();
-            SceneManager.LoadScene("Level 1");
-        }
         
-        StartLevel();
     }
-    
+
 
     public void ScorePoints()
     {
+        collectedPoints++;
+        _txtPoints.text = "Points : " + collectedPoints * 100;
 
-        if (level == 1)
+        if (collectedPoints == points)
         {
-            if (points < 5)
-            {
-                points++;
-                _txtPoints.text = "Points : " + points * 100;
-            }
-            else
-            {
-                points++;
-                _txtPoints.text = "Points : " + points * 100;
-                winner = true;
+            LoadNextLevel();
+            collectedPoints = 0;
+        }
+    }
 
-                if (!PlayerPrefs.HasKey("level"))
-                {
-                    level = 2;
-                    PlayerPrefs.SetInt("level", level);
-                    PlayerPrefs.Save();
-                }
-                
-                NextLevel();
-            }
-        }
-        else if (level == 2)
-        {
-            if (points < 5)
-            {
-                points++;
-                _txtPoints.text = "Points : " + points * 100;
-            }
-            else
-            {
-                points++;
-                _txtPoints.text = "Points : " + points * 100;
-                winner = true;
-            }
-        }
+    private void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
     }
 }
